dc6792959da38aaff9fe2b63c7b48b9a6231eba5
Gabriela Queiroz <gabiqss1@gmail.com>
2023-11-29 05:06:10 -0300

    Initial commit

---
+++ lyricslen.py
@@ -0,0 +1,100 @@
+lyrics = [
+
+          "and I'm so sick of seventeen, where's my fucking teenage dream? if someone tells me one more time 'enjoy your youth', I'm gonna cry",
+          "God, it's brutal out here",
+          "I'm so insecure, I think, that I'll die before I drink",
+
+          "it took you two weeks to go off and date her, guess you didn't cheat, but you're still a traitor",
+
+          "you were so excited for me to finally drive up to your house, but today I drove through the suburbs crying 'cause you weren't around",
+          "'cause you said forever, now I drive alone past your street",
+          "all my friends are tired of hearing how much I miss you, but I kinda feel sorry for them, 'cause they'll never know you the way that I do, yeah",
+
+          "you got me fucked up in the head, boy",
+          "it's always one step forward and three steps back. do you love me, want me, hate me, boy? I don't understand",
+
+          "so when you gonna tell her, that we did that, too? she thinks it's special, but it's all reused",
+          "that was our place, I found it first, I made the jokes, you tell to her when she's with you.", 
+          "do you get déjà vu when she's with you?",
+          "I know you get déjà vu",
+
+          "good for you, you look happy and healthy, not me, if you ever cared to ask", 
+          "well, good for you, I guess you moved on really easily, you found a new girl and it only took a couple weeks",
+          "I guess that therapist I found for you, she really helped. now you can be a better man for your brand-new girl",
+          "maybe I'm too emotional, but your apathy's like a wound in salt. maybe I'm too emotional, or maybe you never cared at all",
+
+          "'cause all I ever wanted was to be enough for you",
+
+          "you've moved on, found someone new, one more girl who brings out the better in you",
+          "oh, I hope you're happy, but not like how you were with me. I'm selfish, I know, I can't let you go. so find someone great, but don't find no one better. I hope you're happy, but don't be happier",
+          "but she's beautiful, she looks kind, she probably gives you butterflies",
+
+          "I'm so sick of myself",
+          "got a pretty face, a pretty boyfriend, too. I wanna be you so bad and I don't even know you",
+
+          "the things I did, just so I could call you mine. the things you did, well, I hope I was your favorite crime",
+          "I wonder if you're around, 'cause you know that I'd do it all again",
+          "It's bittersweet to think about the damage that we do, 'cause I was going down, but I was doing it with you",
+          "yeah, everything we broke, and all the trouble that we made, but I say that I hate you with a smile on my face",
+
+          "but if you're out there, I hope that you're okay",
+          "we don't talk much but I just gotta say, I miss you, and I hope that you're okay",
+
+          "I am light as a feather, I'm as stiff as a board. I pay attention to things that most people ignore",
+          "and I make light of the darkness. I've got Sun in my motherfuckin' pocket, best believe",
+          "got what you can't resist, I'm a perfect all-american bitch, with perfect all-american lips, and perfect all-american hips",
+          "all the time, I'm grateful all the time, I'm sexy and I'm kind, I'm pretty when I cry",
+          "I scream inside to deal with it, like: aaah. like: AAAAAAAAAAAAH",
+
+          "and I know we're done, I know we're through, but, God, when I look at you my brain goes: AAAAAAH",
+          "seeing you tonight, it's a bad idea, right? seeing you tonight... fuck it, it's fine",
+          "yes, I know that he's my ex, but can't two people reconnect?",
+          "I only see him as a friend, the biggest lie I ever said",
+          "I only see him as a friend, I just tripped and fell into his bed",
+          "I know I should stop, but I can't, and I told my friends I was asleep, but I never said where or in whose sheets",
+          "my brain goes: AAAAAH. can't hear my thoughts (I cannot hear my thoughts), like blah-blah-blah (blah-blah-blah-blah-blah-blah)",
+
+          "I used to think I was smart, but you made me look so naive",
+          "bloodsucker, famefucker, bleedin' me dry like a goddamn vampire",
+          "you called them crazy, God, I hate the way I called 'em crazy too",
+
+          "the sweetest torture one could bear",
+          "yeah, I despise my rotten mind and how much it worships you",
+
+          "and I hate all my clothes, feels like my skin doesn't fit right over my bones",
+          "each time I step outside, it's social suicide",
+          "it's social suicide, wanna curl up and die",
+          "thought your mom was your wife, \ncalled you the wrong name twice, \ncan't think of a third line, \nla-la-la-la-la-la",
+
+          "I'm so tired of being the girl that I am",
+          "and I'm playing the victim so well in my head, but it's me who's been making the bed",
+          "well, sometimes I feel like I don't wanna be where I am",
+
+          "two plus two equals five, and I'm the love of your life",
+          "'cause loving you is loving every argument you held over my head, brought up the girls you could have instead, said I was too young, I was too soft. can't take a joke, can't get you off. oh, why do I do this? I look so stupid",
+          "logical, logical, love is never logical",
+          "I know I could've stopped it all, God, why didn't I stop it all?",
+
+          "I met a guy in the summer and I left him in the spring",
+          "I wanna get him back, I wanna make him really jealous, wanna make him feel bad",
+          "yeah, I pour my little heart out, but as I'm hitting: send, I picture all the faces of my disappointed friends",
+          "I wanna meet his mom, just to tell her son sucks",
+
+          "God, love's fucking embarrassing",
+          "and I consoled you while you cried, over your ex-girlfriend's new guy, my God, how could I be so stupid?",
+          "I give up, give up, I give up everything, I placed my bets and it's not worth anything",
+
+          "and I doubt you ever think about, the damage that you did",
+          "and I try to be tough, but I wanna scream, how could anybody do the things you did so easily?",
+          "and I say I don't care, I say that I'm fine, but you know I can't let it go, I've tried, I've tried, I've tried for so long",
+
+          "when pretty isn't pretty enough",
+
+          "but I fear that they already got all the best parts of me, and I'm sorry that I couldn't always be your teenage dream",
+          "will I spend all the rest of my years wishing I could go back?",
+          "yeah, they all say that it gets better, it gets better, but what if I don't?",
+          "oh, they all say that it gets better, it gets better the more you grow",
+          "(Is this recording?)\n(Of course it is)"
+          ]
+
+print(len(lyrics))
