8192cf3be162a0ec864ef83db1b63cabed5f8136
Gabriela Queiroz <gabiqss1@gmail.com>
2023-11-28 22:28:58 -0300

    update: change lib
    changing from tweepy to requests-oauthlib

---
+++ app (oauth-lib).py
@@ -0,0 +1,83 @@
+from config import CONSUMER_KEY, CONSUMER_SECRET
+from requests_oauthlib import OAuth1Session
+import json
+import time
+import random
+from lyrics import lyrics
+
+consumer_key = CONSUMER_KEY
+consumer_secret = CONSUMER_SECRET
+
+lyrics = lyrics
+
+intervalo_entre_postagens = 30 * 60
+historico_indices = []
+
+# Código para obter os tokens de acesso
+request_token_url = "https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write"
+oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
+
+try:
+    fetch_response = oauth.fetch_request_token(request_token_url)
+except ValueError:
+    print("There may have been an issue with the consumer_key or consumer_secret you entered.")
+
+resource_owner_key = fetch_response.get("oauth_token")
+resource_owner_secret = fetch_response.get("oauth_token_secret")
+print("Got OAuth token: %s" % resource_owner_key)
+
+# Obter autorização
+base_authorization_url = "https://api.twitter.com/oauth/authorize"
+authorization_url = oauth.authorization_url(base_authorization_url)
+print("Please go here and authorize: %s" % authorization_url)
+verifier = input("Paste the PIN here: ")
+
+# Obter o token de acesso
+access_token_url = "https://api.twitter.com/oauth/access_token"
+oauth = OAuth1Session(
+    consumer_key,
+    client_secret=consumer_secret,
+    resource_owner_key=resource_owner_key,
+    resource_owner_secret=resource_owner_secret,
+    verifier=verifier,
+)
+oauth_tokens = oauth.fetch_access_token(access_token_url)
+
+access_token = oauth_tokens.get("oauth_token")
+access_token_secret = oauth_tokens.get("oauth_token_secret")
+
+while True:
+    # Seleciona aleatoriamente um índice da lista de letras
+    while True:
+        indice_selecionado = random.randint(0, len(lyrics) - 1)
+        # Verifica se o índice não está no histórico dos últimos 5 índices
+        if indice_selecionado not in historico_indices:
+            break
+
+    # Adiciona o índice selecionado ao histórico
+    historico_indices.append(indice_selecionado)
+    # Mantém apenas os últimos 5 índices no histórico
+    historico_indices = historico_indices[-5:]
+
+    texto_do_tweet = lyrics[indice_selecionado]
+
+    # Código para fazer a postagem
+    oauth = OAuth1Session(
+        consumer_key,
+        client_secret=consumer_secret,
+        resource_owner_key=access_token,
+        resource_owner_secret=access_token_secret,
+    )
+
+    payload = {"text": texto_do_tweet}
+    response = oauth.post("https://api.twitter.com/2/tweets", json=payload)
+
+    if response.status_code != 201:
+        raise Exception(
+            "Request returned an error: {} {}".format(response.status_code, response.text)
+        )
+
+    print(f"Tweet enviado com sucesso: {texto_do_tweet}")
+
+    # Aguarda o intervalo antes de fazer a próxima postagem
+    time.sleep(intervalo_entre_postagens)
---
+++ lyrics.py
@@ -0,0 +1,24 @@
+lyrics = [
+          "good for you, you look happy and healthy, not me, if you ever cared to ask", 
+          "well, good for you, I guess you moved on really easily, you found a new girl and it only took a couple weeks",
+          "I guess that therapist I found for you, she really helped, now you can be a better man for your brand-new girl",
+          "maybe I'm too emotional, but your apathy's like a wound in salt. maybe I'm too emotional, or maybe you never cared at all",
+          
+          "and I'm so sick of seventeen, where's my fucking teenage dream? if someone tells me one more time 'enjoy your youth', I'm gonna cry",
+
+          "you were so excited for me to finally drive up to your house, but today I drove through the suburbs crying 'cause you weren't around",
+          "'cause you said forever, now I drive alone past your street",
+          "all my friends are tired of hearing how much I miss you, but I kinda feel sorry for them, 'cause they'll never know you the way that I do, yeah",
+          
+          "it took you two weeks to go off and date her, guess you didn't cheat, but you're still a traitor",
+          
+          "you got me fucked up in the head, boy",
+          "it's always one step forward and three steps back do you love me, want me, hate me, boy? I don't understand",
+
+          "so when you gonna tell her, that we did that, too? she thinks it's special, but it's all reused",
+          "that was our place, I found it first, I made the jokes, you tell to her when she's with you.", 
+          "do you get déjà vu when she's with you?",
+          "I know you get déjà vu",
+
+          "",
+          ]
