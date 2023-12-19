e326061bb2a00bb71542286d2388ff66adce3dd0
Gabriela Queiroz <gabiqss1@gmail.com>
2023-12-19 17:25:22 -0300

    update

--- app.py
+++ app.py
@@ -1,92 +1,23 @@
-import os
-import sys
-# Adiciona o diretório do script ao PATH
-current_dir = os.path.dirname(os.path.realpath(__file__))
-parent_dir = os.path.dirname(current_dir)
-sys.path.extend([current_dir, parent_dir])
-print("sys.path:", sys.path)
-print("current_dir:", current_dir)
-from config import CONSUMER_KEY, CONSUMER_SECRET, PIN
-from requests_oauthlib import OAuth1Session
-import json
-import time
+import tweepy
+from config import *
 import random
 from lyrics import lyrics
 
-consumer_key = CONSUMER_KEY
-consumer_secret = CONSUMER_SECRET
-
+# lista de letras
 lyrics = lyrics
 
-intervalo_entre_postagens = 2 * 60 * 60
-historico_indices = []
+def lambda_handler(event, context):
 
-# Código para obter os tokens de acesso
-request_token_url = "https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write"
-oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
-
-try:
-    fetch_response = oauth.fetch_request_token(request_token_url)
-except ValueError:
-    print("There may have been an issue with the consumer_key or consumer_secret you entered.")
-
-resource_owner_key = fetch_response.get("oauth_token")
-resource_owner_secret = fetch_response.get("oauth_token_secret")
-print("Got OAuth token: %s" % resource_owner_key)
-print("Got OAuth token secret: %s" % resource_owner_secret)
-
-# Obter autorização
-base_authorization_url = "https://api.twitter.com/oauth/authorize"
-authorization_url = oauth.authorization_url(base_authorization_url)
-print("Please go here and authorize: %s" % authorization_url)
-verifier = input("Paste the PIN here: ")
-
-# Obter o token de acesso
-access_token_url = "https://api.twitter.com/oauth/access_token"
-oauth = OAuth1Session(
-    consumer_key,
-    client_secret=consumer_secret,
-    resource_owner_key=resource_owner_key,
-    resource_owner_secret=resource_owner_secret,
-    verifier=verifier,
-)
-oauth_tokens = oauth.fetch_access_token(access_token_url)
-
-access_token = oauth_tokens.get("oauth_token")
-access_token_secret = oauth_tokens.get("oauth_token_secret")
-
-while True:
-    # Seleciona aleatoriamente um índice da lista de letras
-    while True:
-        indice_selecionado = random.randint(0, len(lyrics) - 1)
-        # Verifica se o índice não está no histórico dos últimos 5 índices
-        if indice_selecionado not in historico_indices:
-            break
-
-    # Adiciona o índice selecionado ao histórico
-    historico_indices.append(indice_selecionado)
-    # Mantém apenas os últimos 50 índices no histórico
-    historico_indices = historico_indices[-50:]
-
+    # seleciona um índice aleatório
+    indice_selecionado = random.randint(0, len(lyrics) - 1)
+    
+    # autenticação com twitter
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    client = tweepy.API(auth)
+    
+    # texto do tweet
     texto_do_tweet = lyrics[indice_selecionado]
-
-    # Código para fazer a postagem
-    oauth = OAuth1Session(
-        consumer_key,
-        client_secret=consumer_secret,
-        resource_owner_key=access_token,
-        resource_owner_secret=access_token_secret,
-    )
-
-    payload = {"text": texto_do_tweet}
-    response = oauth.post("https://api.twitter.com/2/tweets", json=payload)
-
-    if response.status_code != 201:
-        raise Exception(
-            "Request returned an error: {} {}".format(response.status_code, response.text)
-        )
-
-    print(f"Tweet enviado com sucesso: {texto_do_tweet}")
-
-    # Aguarda o intervalo antes de fazer a próxima postagem
-    time.sleep(intervalo_entre_postagens)
+    
+    # postar!
+    client.update_status(status=texto_do_tweet)
--- lyrics.py
+++ lyrics.py
@@ -1,15 +1,18 @@
 lyrics = [
-
+          
+          # brutal
           "and I'm so sick of seventeen, where's my fucking teenage dream? if someone tells me one more time 'enjoy your youth', I'm gonna cry",
           "God, it's brutal out here",
           "I'm so insecure, I think, that I'll die before I drink",
           "got a broken ego, broken heart, and God, I don't even know where to start",
           "they say these are the golden years but I wish that I could disappear",
-
+          
+          # traitor
           "it took you two weeks to go off and date her, guess you didn't cheat, but you're still a traitor",
           "ain't it funny? remember I brought her up and you told me I was paranoid?",
           "and ain't it funny how you ran to her the second that we called it quits?",
-
+          
+          # drivers license
           "you were so excited for me to finally drive up to your house, but today I drove through the suburbs crying 'cause you weren't around",
           "'cause you said forever, now I drive alone past your street",
           "today I drove through the suburbs and pictured I was driving home to you",
@@ -17,10 +20,12 @@ lyrics = [
           "can't drive past the places we used to go to, 'cause I still fucking love you, babe",
           "guess you didn't mean what you wrote in that song about me",
           "all my friends are tired of hearing how much I miss you, but I kinda feel sorry for them, 'cause they'll never know you the way that I do, yeah",
-
+          
+          # 1 step forward, 3 steps back
           "you got me fucked up in the head, boy",
           "it's always one step forward and three steps back. do you love me, want me, hate me, boy? I don't understand",
-
+          
+          # deja vu
           "so when you gonna tell her, that we did that, too? she thinks it's special, but it's all reused",
           "trading jackets, laughing 'bout how small it looks on you",
           "do you call her, almost say my name? 'cause let be honest, we kinda do sound the same",
@@ -28,43 +33,51 @@ lyrics = [
           "that was our place, I found it first, I made the jokes, you tell to her when she's with you.", 
           "do you get déjà vu when she's with you?",
           "I know you get déjà vu",
-
+          
+          # good 4 u
           "good for you, you look happy and healthy, not me, if you ever cared to ask", 
           "well, good for you, I guess you moved on really easily, you found a new girl and it only took a couple weeks",
           "remember when you said that you wanted to give me the world?",
           "remember when you swore to god I was the only person who ever got you? well, screw that, and screw you",
           "I guess that therapist I found for you, she really helped. now you can be a better man for your brand-new girl",
           "maybe I'm too emotional, but your apathy's like a wound in salt. maybe I'm too emotional, or maybe you never cared at all",
-
+          
+          # enough for you
           "'cause all I ever wanted was to be enough for you",
           "now I don't want your sympathy I just want myself back before",
           "tried so hard to be everything that you liked, just for you to say you're not the compliment type",
-
+          
+          # happier
           "you've moved on, found someone new, one more girl who brings out the better in you",
           "oh, I hope you're happy, but not like how you were with me. I'm selfish, I know, I can't let you go. so find someone great, but don't find no one better. I hope you're happy, but don't be happier",
           "but she's beautiful, she looks kind, she probably gives you butterflies",
           "and do you tell her she's the most beautiful girl you've ever seen? an eternal love bullshit you know you'll never mean. remember when I believed you meant it when you said it first to me?",
-
+          
+          # jealousy, jealousy
           "I'm so sick of myself",
           "got a pretty face, a pretty boyfriend, too. I wanna be you so bad and I don't even know you",
           "jealousy, jealousy, started following me",
           "I kinda wanna throw my phone across the room, 'cause all I see are girls too good to be true with paper white teeth and perfect bodies",
-
+          
+          # favorite crime
           "the things I did, just so I could call you mine. the things you did, well, I hope I was your favorite crime",
           "I wonder if you're around, 'cause you know that I'd do it all again",
           "It's bittersweet to think about the damage that we do, 'cause I was going down, but I was doing it with you",
           "yeah, everything we broke, and all the trouble that we made, but I say that I hate you with a smile on my face",
           "well, I hope I was your favorite crime, your favorite crime, your favorite crime, 'cause, baby, you were mine",
-
+          
+          # hope ur ok
           "but if you're out there, I hope that you're okay",
           "we don't talk much but I just gotta say, I miss you, and I hope that you're okay",
-
+          
+          # all-american bitch
           "I am light as a feather, I'm as stiff as a board. I pay attention to things that most people ignore",
           "and I make light of the darkness. I've got Sun in my motherfuckin' pocket, best believe",
           "got what you can't resist, I'm a perfect all-american bitch, with perfect all-american lips, and perfect all-american hips",
           "all the time, I'm grateful all the time, I'm sexy and I'm kind, I'm pretty when I cry",
           "I scream inside to deal with it, like: aaah. like: AAAAAAAAAAAAH",
-
+          
+          # bad idea right?
           "and I know we're done, I know we're through, but, God, when I look at you my brain goes: AAAAAAH",
           "seeing you tonight, it's a bad idea, right? seeing you tonight... fuck it, it's fine",
           "yes, I know that he's my ex, but can't two people reconnect?",
@@ -72,62 +85,73 @@ lyrics = [
           "I only see him as a friend, I just tripped and fell into his bed",
           "I know I should stop, but I can't, and I told my friends I was asleep, but I never said where or in whose sheets",
           "my brain goes: AAAAAH. can't hear my thoughts (I cannot hear my thoughts), like blah-blah-blah (blah-blah-blah-blah-blah-blah)",
-
+          
+          # vampire
           "I used to think I was smart, but you made me look so naive",
           "bloodsucker, famefucker, bleedin' me dry like a goddamn vampire",
           "you called them crazy, God, I hate the way I called 'em crazy too",
           "the way you sold me for parts, as you sunk your teeth into me, oh",
-
+          
+          # lacy
           "the sweetest torture one could bear",
           "yeah, I despise my rotten mind and how much it worships you",
           "Lacy, oh, Lacy, skin like puff pastry",
           "people are people, but it's like you're made of angel dust",
           "Lacy, oh, Lacy, it's like you're out to get me. you poison every little thing that I do",
-
+          
+          # ballad of a homeschooled girl
           "and I hate all my clothes, feels like my skin doesn't fit right over my bones",
           "each time I step outside, it's social suicide",
           "it's social suicide, wanna curl up and die",
           "when I'm alone, I'm fine, but don't let me out at night, it's social suicide",
           "thought your mom was your wife, \ncalled you the wrong name twice, \ncan't think of a third line, \nla-la-la-la-la-la",
-
+          
+          # making the bed
           "I'm so tired of being the girl that I am",
           "and I'm playing the victim so well in my head, but it's me who's been making the bed",
           "well, sometimes I feel like I don't wanna be where I am",
-
+          
+          # logical
           "two plus two equals five, and I'm the love of your life",
           "'cause loving you is loving every argument you held over my head, brought up the girls you could have instead, said I was too young, I was too soft. can't take a joke, can't get you off. oh, why do I do this? I look so stupid",
           "logical, logical, love is never logical",
           "I know I could've stopped it all, God, why didn't I stop it all?",
-
+          
+          # get him back!
           "I met a guy in the summer and I left him in the spring",
           "I wanna get him back, I wanna make him really jealous, wanna make him feel bad",
           "yeah, I pour my little heart out, but as I'm hitting: send, I picture all the faces of my disappointed friends",
           "I wanna meet his mom, just to tell her son sucks",
           "I wanna get him back",
-
+          
+          # love is embarrassing
           "God, love's fucking embarrassing",
           "and I consoled you while you cried, over your ex-girlfriend's new guy, my God, how could I be so stupid?",
           "I give up, give up, I give up everything, I placed my bets and it's not worth anything",
-
+          
+          # the grudge
           "and I doubt you ever think about, the damage that you did",
           "and I try to be tough, but I wanna scream, how could anybody do the things you did so easily?",
           "and I say I don't care, I say that I'm fine, but you know I can't let it go, I've tried, I've tried, I've tried for so long",
           "it takes strength to forgive, but I don't feel strong",
           "you built me up to watch me fall, you have everything and you still want more",
 
+          # pretty isn't pretty
           "when pretty isn't pretty enough",
 
+          # teenage dream
           "but I fear that they already got all the best parts of me, and I'm sorry that I couldn't always be your teenage dream",
           "will I spend all the rest of my years wishing I could go back?",
           "yeah, they all say that it gets better, it gets better, but what if I don't?",
           "oh, they all say that it gets better, it gets better the more you grow",
           "(Is this recording?)\n(Of course it is)",
+
+          # can't catch me now
+          "there's snow falling over the city, you thought that it would wash away the bitter taste of my fury and all of the messes you made. yeah, you think that you got away",
+          "shadows of us are still dancing in every room and every hall",
+          "you'll see my face in every place, but you can't catch me now",
+          "through wading grass, the months will pass, you'll feel it all around. I'm here, I'm there, I'm everywhere, but you can't catch me now",
+          "bet you thought I'd never do it, thought it'd go over my head. I bet you figured I'd pass with the winter be something easy to forget. oh, you think I'm gone 'cause I left",
+          "you, you can't, you can't catch me now, I'm coming like a storm into your town. you can't, you can't catch me now, I'm higher than the hopes that you brought down",
+          "yeah, sometimes the fire you founded don't burn the way you'd expect",
           ]
-
-
-print(len(lyrics), "letras")
-
-n = 0
-for i in lyrics:
-    n += 1
-    print('letra n.{}:'.format(n), i)
